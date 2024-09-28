c0df8d04c6733887f78baecadb87391cddd803fd
pineappleboy187 <79086760+pineappleboy187@users.noreply.github.com>
2024-09-28 00:55:57 -0600

    Finished SMS Traccer integration.

--- Campsite_Messaging.py
+++ Campsite_Messaging.py
@@ -1,3 +1,26 @@
-print("hello world")
+#Used for the Traccer sms gateway app.
+import requests
 
-print("I Love Github")
+# config area
+import config
+
+def send(number, message):
+    try:
+        x = requests.post(config.url, json={'to': number, 'message': message}, headers={"Authorization": config.api})
+        if x.status_code == 200:
+            return True
+        else:
+            return False
+
+    except Exception as error:
+        print("An exception occurred:", error)
+        return False
+
+
+def main():
+    if send("+15873359944", "Hello"):
+        print("Message sent")
+
+
+main()
+
